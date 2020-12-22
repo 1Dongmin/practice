cd6dde98e22de5d21b0a87d7f0386c6983373d5b
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2020-12-22 16:30:54 +0900

    Update square.c

--- Problem/square.c
+++ Problem/square.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 #define ll long long
-int main () {
-	unsigned ll min, max, count = 0, squre = 0;
-	scanf("%llu", &min);
-	scanf("%llu", &max);
-	printf("\n%llu %llu \n", min, max); //input check
-	for(; min < max; min++){
-		for(; (squre * squre) <= min; squre++){
-			if ((squre * squre) != min){
-				count++;
-				printf("count! count: %llu suqre: %llu\n",count,squre * squre) //count check
-				
-			}
+int main ()
+{
+	ll min, max, count = 0, square = 0;
+	scanf("%lld %lld", &min, &max);
+	for(square = 1; square <= max; square++)
+	{
+		che1:
+		if ((square * square) <= (min-1)){square++;goto che1;}
+		if (min <= (square * square) && (square * square) <= max) 
+		{
+			count++;
+			printf("count! square: %lld max:%lld \n", square, max);
 		}
+		printf("min: %lld square: %lld\n", min, square);
 	}
-	printf("\n%llu", count);
+	printf("count: %lld\n", count);
+	printf("%lld",(max - min + 1)- count);
 	return 0;
 }
