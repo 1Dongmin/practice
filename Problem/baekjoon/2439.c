1d92bb15f815ef094af8dd4eea2477aba741086d
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-01-25 16:40:22 +0900

    정답 v 1.0

--- Problem/baekjoon/2439.c
+++ Problem/baekjoon/2439.c
@@ -3,10 +3,10 @@ int main(){
     int a;
     scanf("%d", &a);
     for(int i = 1; i <= a; i++){
-        for(int b = i; (a - b) >= 0; b++){
+        for(int b = i; (a - b) > 0; b++){
             printf(" ");
         }
-        for(int k = a; k >= 0; k--){
+        for(int k = 1; k <= i; k++){
             printf("*");
         }
         printf("\n");
