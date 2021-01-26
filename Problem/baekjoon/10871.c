2379e4ae827818d7c4eeff6e34066a9c6169d458
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-01-26 13:13:08 +0900

    10871 미완성

--- Problem/baekjoon/10871.c
+++ Problem/baekjoon/10871.c
@@ -4,11 +4,10 @@ int main()
 {
     int na, x;
     scanf("%d %d", &na, &x);
-    int *n = malloc(sizeof(int) * size);
+    int *n = malloc(sizeof(int) * na);
     for(int i = 0; i < na; i++)
     {
         scanf("%d", n[i]);
-        //printf("%d",n[i]);
     }
     for (int k = 0; k < na; k++){
         if(n[k] < x){
