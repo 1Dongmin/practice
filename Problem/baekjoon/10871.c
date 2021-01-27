3d256339458a545df79c030fbc422f6edd5e4699
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-01-27 09:32:44 +0900

    정답 v 1.0

--- Problem/baekjoon/10871.c
+++ Problem/baekjoon/10871.c
@@ -2,16 +2,18 @@
 #include <stdlib.h>
 int main()
 {
-    int na, x;
+    int na, x, temp;
     scanf("%d %d", &na, &x);
-    int *n = malloc(sizeof(int) * na);
-    for(int i = 0; i < na; i++)
+    int* n = (int*)malloc(sizeof(int) * na);
+    for (int i = 0; i < na; i++)
     {
-        scanf("%d", n[i]);
+        scanf("%d", &temp);
+        n[i] = temp;
     }
-    for (int k = 0; k < na; k++){
-        if(n[k] < x){
-            printf("%d", n[k]);
+    for (int k = 0; k < na; k++) {
+        if (n[k] < x) {
+            printf("%d ", n[k]);
         }
     }
+    free(n);
 }
