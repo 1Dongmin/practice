692c3ce300989e78a1f43a6d3c9810accfaa3cd8
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-02-01 10:40:35 +0900

    3자리의 수 3개를 입력받고 곱한 숫자가 각각 몇 개가 나오는지 구하는 프로그램

--- Problem/baekjoon/2577.cpp
+++ Problem/baekjoon/2577.cpp
@@ -1,17 +1,30 @@
 #include <stdio.h>
 int main(){
-    int a, b, c, sum, tmp[10] = {0}, num[10] = {0}, k = 0, tmp1 = 0;
+    int a, b, c, sum, tmp[10] = {0}, num[10] = {0}, k = 9, tmp1 = 0, checknum1 = 0;
     scanf("%d %d %d", &a, &b, &c);
     sum = a * b * c;
-    for(int i = 1; i <= 100000000; i = i * 10){
-        tmp[k] = sum / i
-        tmp1 = sum / i;
-        
-        k++;
+    tmp1 = sum;
+    for(int i = 0; i <= 9; i++){
+        tmp[k] = tmp1 % 10;
+        tmp1 = tmp1 / 10;
+        if(tmp1 < 1) break;
+        k--;
     }
     for(k = 0; k <= 9; k++){
-        if(k == tmp[k])
-            num[k] = num[k] + 1;
+        if(tmp[k] == 0){
+		if(checknum1 == 0)continue;
+	num[0]++; checknum1++;}
+        else if(tmp[k] == 1){num[1]++; checknum1++;}
+        else if(tmp[k] == 2){num[2]++; checknum1++;}
+        else if(tmp[k] == 3){num[3]++; checknum1++;}
+        else if(tmp[k] == 4){num[4]++; checknum1++;}
+        else if(tmp[k] == 5){num[5]++; checknum1++;}
+        else if(tmp[k] == 6){num[6]++; checknum1++;}
+        else if(tmp[k] == 7){num[7]++; checknum1++;}
+        else if(tmp[k] == 8){num[8]++; checknum1++;}
+        else if(tmp[k] == 9){num[9]++; checknum1++;}
+	
+        //printf("%d",tmp[k]);
     }
-    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n", num[0], num[1], num[2], num[3], num[3], num[4], num[5], num[6], num[7], num[8], num[9]);
+    printf("%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n%d\n", num[0], num[1], num[2], num[3], num[4], num[5], num[6], num[7], num[8], num[9]);
 }
