476832140060431e6c8a0c4f93ce1e69c751d83b
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-02-02 14:54:47 +0900

    나머지를 배열에 저장하고 배열 안에서 서로 다른 수가 몇 개 있는지 구하기

--- Problem/baekjoon/3052.cpp
+++ Problem/baekjoon/3052.cpp
@@ -1,26 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 int main() {
-    int inp[10], tmp[10], remain[10], temp, fin = 0;
+    int inp[10], tmp[10], remain[10], temp, fin = 0, savno = 0;
     for (int i = 0; i <= 9; i++) {
         scanf("%d", &temp);
         inp[i] = temp;
         remain[i] = temp % 42;
-    }
-    for (int i = 0; i <= 9; i++) {
-        if (i == 0) {
-            fin++;
-            tmp[i] = remain[i];
-            continue;
-        }
-        for (int k = 0; k <= i; k++) {
-            if (remain[i] == tmp[k]) {
+        //printf("remain[i] = %d\n", remain[i]);
+        if (i == 0)fin++;
+        for (int k = 0; k < i; k++) {
+            if (remain[k] == remain[i]) {
+                //printf("있던 수 %d\n", remain[i]);
                 break;
             }
-            if (k == i) {
+            if (k == (i - 1)) {
+                //printf("Hit!\n");
                 fin++;
-                break;
             }
         }
     }
-    printf("%d", fin);
+    printf("%d\n", fin);
 }
