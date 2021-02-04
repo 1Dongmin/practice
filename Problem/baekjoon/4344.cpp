ce6f220ad5db655acbb2448bb5d5624160c61e39
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-02-04 17:49:56 +0900

    [미완] 평균 넘는 사람이 몇퍼센트나 되는지 출력

---
+++ Problem/baekjoon/4344.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    int rep, times, score[100], sum = 0, winner = 0;
+    double avr;
+    scanf("%d", &rep);
+    for(int i = 0; i < rep; i++){
+        scanf("%d", &times);
+        for(int k = 0; k < times; k++){
+            scanf("%d", &score[k]);
+            sum += score[k];
+        }
+        avr = ((sum * 1.0) / (times * 1.0));
+        for(int t = 0; t < times; t++){
+            if(avr < score[t]) winner++;
+        }
+        printf("%lf\n", (winner * 1.0) / (times * 1.0) * 100.0);
+        sum = 0, winner = 0, avr = 0;
+    }
+    for(int i = 0; i < times; i++){
+        avr = (sum / times) * 1.0;
+    }
+}
