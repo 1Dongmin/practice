494a058396cd0e2d1d9e33d8d27778d18b709a95
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-02-04 17:51:26 +0900

    [미완] 평균 판독 버전투

--- Problem/baekjoon/4344.cpp
+++ Problem/baekjoon/4344.cpp
@@ -13,7 +13,7 @@ int main(){
         for(int t = 0; t < times; t++){
             if(avr < score[t]) winner++;
         }
-        printf("%lf\n", (winner * 1.0) / (times * 1.0) * 100.0);
+        printf("%lf%%\n", (winner * 1.0) / (times * 1.0) * 100.0);
         sum = 0, winner = 0, avr = 0;
     }
     for(int i = 0; i < times; i++){
