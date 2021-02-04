b1207accddb687dfd02cdf7a08510bdf8d5bb664
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-02-04 13:25:30 +0900

    [미완] OXOX 점수채점

--- Problem/baekjoon/8958.cpp
+++ Problem/baekjoon/8958.cpp
@@ -1,14 +1,15 @@
 #include <stdio.h>
 int main(){
-    int time;
+    int time, score = 0, sum = 0;
     char oxox[80];
     scanf("%d", &time);
     for(int i = 0; i < time; i++){
         scanf("%s", &oxox);
         for(int k = 0; k < 80; k++){
             if(oxox[k] == NULL) break;
-            
+            if(oxox[k] == O){
+                
+            }
         }
     }
-    
 }
