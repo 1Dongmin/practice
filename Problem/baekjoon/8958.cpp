67f339cbccb2e2739c60ae57f41fb4df3ef75c48
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-02-04 15:50:13 +0900

    OXOX 문자열 입력받아 점수 합계 채점

--- Problem/baekjoon/8958.cpp
+++ Problem/baekjoon/8958.cpp
@@ -1,15 +1,21 @@
 #include <stdio.h>
 int main(){
-    int time, score = 0, sum = 0;
-    char oxox[80];
+    int time, stack = 0, sum = 0;
+    char oxox[80], O = 'O', X = 'X';
     scanf("%d", &time);
     for(int i = 0; i < time; i++){
         scanf("%s", &oxox);
-        for(int k = 0; k < 80; k++){
-            if(oxox[k] == NULL) break;
-            if(oxox[k] == O){
-                
+        for(int k = 0; oxox[k] != NULL; k++){
+            if(oxox[k] == 'O'){
+                stack++;
+                sum += stack;
+            }
+            if(oxox[k] == 'X'){
+                stack = 0;
             }
         }
+        printf("%d\n", sum);
+        sum = 0;
+        stack = 0;
     }
 }
