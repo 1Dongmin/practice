17bc10f1583c739ed9baa66e59d47250afa2b7ce
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-02-15 10:18:16 +0900

    [미완] 마지막 NULL 추가

--- Problem/baekjoon/11720.cpp
+++ Problem/baekjoon/11720.cpp
@@ -5,15 +5,13 @@ int main(void)
 	char inpchar[101], temp[101];
 	long long sum = 0 ,size;
 	scanf("%lld", &size);
-	/*for (int i = 0; i < size; i++){
-		scanf("%s", &inpchar[i]);
-
-	}*/
+	//for (int i = 0; i < size; i++)scanf("%s", &inpchar[i]);
+	
 	inpchar[0] = getchar();
 	for (long long k = 0; k < size; k++) {
 		inpchar[k] = getchar();
 		sum += atoll(&inpchar[k]);
-        if (k == size) NULL;
+        if (k == size) inpchar[k] = NULL;
 		//printf("\n###%c###\n", inpchar[k]);
 	}
 	printf("%lld", sum);
