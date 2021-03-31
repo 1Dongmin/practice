ae2ec0df56cb0a6fcc2956641cc6391e0ff1333c
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-03-31 17:02:04 +0900

    문자열로 입력되는 숫자의 합
    stdlib 이건 필요 없을듯

--- Problem/baekjoon/11720.cpp
+++ Problem/baekjoon/11720.cpp
@@ -4,22 +4,14 @@
 int main(void)
 {
 	char inpchar[101], temp[101];
-	long long sum = 0 ,size;
-	scanf("%lld", &size);
-	//for (int i = 0; i < size; i++)scanf("%s", &inpchar[i]);
-	inpchar[0] = getchar();
-	for (long long k = 0; k < size; k++) {
-		//printf("\nListen...");
-		inpchar[k] = getchar();
-		sum += atoll(&inpchar[k]);
-		
-		//printf("\nk: %lld", k);
-        if (k == size - 1) {
-			inpchar[k] = '\0';
-			//printf("\n###  NULL!!!  ###\n");
-		}
-		//printf("\n### times: %u ###\n### data: %c ###\n", k, inpchar[k]);
-	}
-	printf("%lld", sum);
+    int size, sum = 0;
+	scanf("%d", &size,4);
+    scanf("%s",inpchar,1);
+
+    for(int i = 0; i < size; i ++){
+        //printf("%d\n",inpchar[i]-48);
+        sum += inpchar[i] - 48;
+    }
+	printf("%d", sum);
 	return 0;
 }
