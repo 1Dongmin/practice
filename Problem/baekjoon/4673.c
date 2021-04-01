fc114d608802cf889a88db0c659ee3a78a485c13
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-04-01 17:09:27 +0900

    셀프넘버 출력

--- Problem/baekjoon/4673.c
+++ Problem/baekjoon/4673.c
@@ -1,26 +1,34 @@
 #include <stdio.h>
 #include <stdbool.h>
+int culcu(int i);
 
+bool arr[10001] = {false};
 int main(void)
 {
-    bool arr[10001] = {false};
-    int sum = 0, temp = 0;
+    arr[0] = true;
     for(int i = 1; i <= 10000; i++){
-        temp = i;
-        sum = 0;
-        for(;;){
-            if(temp == 0) break;
-            sum += temp % 10;
-            temp = temp / 10;
-        }
-        sum = i + sum;
-        if(sum > 10000)continue;
-        arr[sum] = true;
+        culcu(i);
     }
-    for(int k = 0; k <= 10000; k++){
+    for(int k = 0; k < 10000; k++){
             if(arr[k] == false){
             printf("%d\n", k);
         }
     }
+    return 0;
+}
+int culcu(int i){
+    int sum = 0, temp = 0;
+
+    temp = i;
+    sum = 0;
+    for(;;){
+        if(temp == 0) break;
+        sum += temp % 10;
+        temp = temp / 10;
+    }
+    sum = i + sum;
+    if(sum > 10000)return 0;
+    arr[sum] = true;
+    
     return 0;
 }
