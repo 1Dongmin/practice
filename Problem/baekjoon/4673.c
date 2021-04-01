04d0c3c777e9090e126605b180592f030f52fe80
1Dongmin <43496375+1Dongmin@users.noreply.github.com>
2021-04-01 16:54:58 +0900

    셀프넘버 출력

--- Problem/baekjoon/4673.c
+++ Problem/baekjoon/4673.c
@@ -3,21 +3,24 @@
 
 int main(void)
 {
-    bool arr[20000] = {false};
+    bool arr[10001] = {false};
     int sum = 0, temp = 0;
-    for(int i = 1; i < 10000; i++){
+    for(int i = 1; i <= 10000; i++){
         temp = i;
+        sum = 0;
         for(;;){
             if(temp == 0) break;
             sum += temp % 10;
             temp = temp / 10;
         }
         sum = i + sum;
+        if(sum > 10000)continue;
         arr[sum] = true;
-        if(arr[i] == false){
-            printf("%d\n", i);
+    }
+    for(int k = 0; k <= 10000; k++){
+            if(arr[k] == false){
+            printf("%d\n", k);
         }
-        sum = 0;
     }
     return 0;
 }
